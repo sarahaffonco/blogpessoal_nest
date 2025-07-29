c07b9d0ccdce652e4d9b9a630fcc52e3d0a115dc
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-07-29 17:11:25 -0300

    implementação put

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -36,4 +36,9 @@ export class PostagemService {
   async create(postagem: Postagem): Promise<Postagem> {
     return await this.postagemRepository.save(postagem);
   }
+
+  async update(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id);
+    return await this.postagemRepository.save(postagem);
+  }
 }
